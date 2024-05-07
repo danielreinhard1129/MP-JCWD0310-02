ddc6cbcb929e992d32a22de635170fa98720e76e
SDHZed <83025970+sdhz980@users.noreply.github.com>
2024-05-07 19:34:35 +0700

    Fix landing page and merge to develop (#11)
    * feat: add landing-page
    
    * feat: add login and register authentication
    
    * feat: add register page
    
    * feat: done setup prisma schema database (#5)
    
    * feat: add landing page
    
    * Delete apps/api/src/controllers/sample.controller.ts
    
    Fix conflict
    
    * Delete apps/api/src/routers/sample.router.ts
    
    Fix conflict
    
    * feat: fix
    
    * fix: fix conflict
    
    * feat: add landing page
    
    ---------
    
    Co-authored-by: tiarmm <septiana.rapihmaharani5@gmail.com>

--- apps/web/src/app/register/page.tsx
+++ apps/web/src/app/register/page.tsx
@@ -1,5 +1,4 @@
 'use client';
-<<<<<<< HEAD
 
 import Forminput from '@/components/Forminput';
 import { Button } from '@/components/ui/button';
@@ -80,67 +79,7 @@ const Register = () => {
         </Card>
       </div>
     </main>
-=======
-const Register = () => {
-  const { register } = useRegister();
-
-  const formik = useFormik({
-    initialValues: {
-      userName: '',
-      email: '',
-      password: '',
-    },
-    onSubmit(values) {
-      register(values);
-    },
-  });
-
-  const Form: FormCard = {
-    InputForm: [
-      {
-        name: 'firstName',
-        label: 'First Name',
-        type: 'text',
-        inputLabel: 'Input your First Name',
-      },
-      {
-        name: 'lastName',
-        label: 'Last Name',
-        type: 'text',
-        inputLabel: 'Input your Last Name',
-      },
-      {
-        name: 'email',
-        label: 'Email',
-        type: 'text',
-        inputLabel: 'Input your Email',
-      },
-      {
-        name: 'password',
-        label: 'Password',
-        type: 'password',
-        inputLabel: 'Input your Password',
-      },
-    ],
-
-    button: { buttonLabel: 'Register' },
-    formik: formik,
-  };
-
-  return (
-    <div className="w-full h-full justify-center p-10 bg-slate-500">
-      <FormCard
-        InputForm={Form.InputForm}
-        button={Form.button}
-        formik={Form.formik}
-      />
-    </div>
->>>>>>> testing
   );
 };
 
 export default Register;
-<<<<<<< HEAD
-
-=======
->>>>>>> testing
